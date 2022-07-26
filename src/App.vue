717d0d8c896ab4bcfb7fb23ac35b02727fd46cf5
Тихомиров Григорий Павлович <gptikhomi1@mts.ru>
2022-07-26 22:14:38 +0300

    Настройка проекта

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <app-header />
+    <index-page />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from "@/components/AppHeader.vue";
+import IndexPage from "./pages/IndexPage.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld,
+    AppHeader,
+    IndexPage,
   },
 });
 </script>
 
 <style lang="scss">
+@import "@/assets/styles/global.scss";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inter", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <img src="../assets/logo.png" alt="logo" class="logo" />
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "AppHeader",
+});
+</script>
+
+<style scoped lang="scss">
+.header {
+  height: 60px;
+  padding: 0 9em;
+  display: flex;
+  align-items: center;
+  background-color: #c4c4c4;
+}
+
+.logo {
+  height: 47px;
+}
+</style>
