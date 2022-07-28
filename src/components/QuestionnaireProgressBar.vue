023d56e08fa889ce82aed286bdeebd4cf424d355
Тихомиров Григорий Павлович <gptikhomi1@mts.ru>
2022-07-28 23:40:25 +0300

    Добавил компонент прогресс бара

---
+++ src/components/QuestionnaireProgressBar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="progress-bar">
+    <div v-for="dot in dots" :key="dot" class="dot-container">
+      <div class="dot" :class="{ active: dot <= current }" />
+      <div v-if="dot !== dots.length" class="dot-bar" :class="{ passed: dot < current }" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+interface Data {
+  dots: number[];
+  dotSizePx: number;
+}
+
+export default Vue.extend({
+  name: "QuestionnaireProgressBar",
+  props: {
+    total: {
+      type: Number,
+      required: true,
+    },
+    current: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data: function (): Data {
+    return {
+      dots: [...Array(this.total).keys()],
+      dotSizePx: 21,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/styles/variables.scss";
+
+.progress-bar {
+  display: flex;
+}
+
+.dot-container {
+  display: flex;
+  align-items: center;
+
+  &:not(:last-of-type) {
+    flex-grow: 1;
+  }
+}
+
+.dot {
+  width: 21px;
+  height: 21px;
+  background: #c4c4c4;
+  border: 2px solid #b3b3b3;
+  border-radius: 50%;
+
+  &.active {
+    background: #8cd6ff;
+    border: 2px solid $color-primary;
+  }
+}
+
+.dot-bar {
+  flex-grow: 1;
+  height: 3px;
+  background-color: #b3b3b3;
+
+  &.passed {
+    background-color: $color-primary;
+  }
+}
+</style>
